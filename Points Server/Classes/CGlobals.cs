26025b6b97ef5ca09535368249a4a1b7d887a22d
sempressimo <sempressimo@hotmail.com>
2015-06-12 07:45:55 -0400

    Send email methods.

--- Points Server/Classes/CGlobals.cs
+++ Points Server/Classes/CGlobals.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 
 namespace Points_Server
@@ -15,5 +17,47 @@ namespace Points_Server
         static public int Count_New_RO = 0;
         static public int Count_Repeated_RO = 0;
         static public int Count_Rejected_RO = 0;
+
+        static private string CreateEmailBody(string Customer, string Points)
+        {
+            return "this is my test email body";
+        }
+
+        static public string SendEmail(string To, string CustomerName, string Balance)
+        {
+            try
+            {
+                //MailMessage mail = new MailMessage("ismael.placa@gmail.com", r["email"].ToString());
+                MailMessage mail = new MailMessage(Properties.Settings.Default.From, To);
+
+                NetworkCredential basicCredential = new NetworkCredential("username", Properties.Settings.Default.Password);
+
+                SmtpClient client = new SmtpClient();
+                client.Port = Convert.ToInt32(Properties.Settings.Default.Port);
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                if (Properties.Settings.Default.UseDefaultCredentials)
+                {
+                    client.UseDefaultCredentials = true;
+                }
+                else
+                {
+                    client.UseDefaultCredentials = false;
+                    client.Credentials = basicCredential;
+                }
+
+                client.Host = Properties.Settings.Default.Smtp;
+                mail.Subject = "Utilice sus puntos en Autocentro Toyota";
+                mail.Body = CreateEmailBody(CustomerName, Balance);
+
+                client.Send(mail);
+
+                return "Sent";
+            }
+            catch (Exception E)
+            {
+                return E.Message;                
+            }
+        }
     }
 }
